11308def9f8ba68f05135de591c3346238a97512
neoaez <jeremy@munkywarestudios.com>
2017-07-11 19:25:46 -0500

    testing beepboop persist storage

--- events/help.js
+++ events/help.js
@@ -7,11 +7,5 @@ module.exports = (app, text) => {
 
   // response to the user typing "help"
   slapp.message('help', ['mention', 'direct_message'], (msg) => {
-    msg.say(helpText)//,
-
-    // testing persist data
-    kv.set('u::c::jenkins', { name: 'Jenkins', thumb: 'https://u8921732.dl.dropboxusercontent.com/u/8921732/slack/thumbs/ar_jenkins.png' }, function (err) {
-      [TO DO] handle error
-    })
-  })
+    msg.say(helpText)
 }
