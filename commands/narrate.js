704316ad1c52fcd0d19493cabe88326bcbe25a51
neoaez <jeremy@munkywarestudios.com>
2017-07-11 19:54:19 -0500

    testing beepboop persist storage

--- commands/narrate.js
+++ commands/narrate.js
@@ -8,12 +8,37 @@ module.exports = (app, text) => {
   // Slash Command: ... 
   slapp.command('/narrate', (msg) => {
 
+    var userID = msg.body.event.user
+    console.info(`user: ${userID}`)
+   
+    var npcName = null
+    var npcText = null
+
+    if (msg.body.event.text.indexOf('|') != -1) {
+      var commandParameters = msg.body.event.text.split('|')
+      npcName = commandParameters[0]
+      npcText = commandParameters[1]
+    } else {
+        npcText = msg.body.event.text
+    }
+
     //testing persist data
-    kv.get('u::c::jenkins', function (err, val) {
+    kv.get(`${userID}::NPC::${npcName}`, function (err, val) {
       // [TO DO] handle error
-      msg.say([
-        `Data retrieved: ${val.thumb}`,
-      ])   
+      msg.say({
+        //response_type: 'in_channel',
+        //text: 'Check out this amazing attachment! :confetti_ball: ',
+        attachments: [{
+          text: `${npcText}`,
+          title: `${val.name}`,
+          //image_url: 'https://storage.googleapis.com/beepboophq/_assets/bot-1.22f6fb.png',
+          //title_link: 'https://beepboophq.com/',
+          //color: '#7CD197'
+          color: "#4bbff4",
+          mrkdwn_in: ["text", "pretext"],
+          thumb_url: `${val.thumb}`
+        }]
+      })   
     })
   })
 }
--- commands/npc.js
+++ commands/npc.js
@@ -8,9 +8,23 @@ module.exports = (app, text) => {
   // Slash Command: ... 
   slapp.command('/npc', (msg) => {
 
+    var userID = msg.body.event.user
+    console.info(`user: ${userID}`)
+   
+    var npcName = "SomeRandomNPCName"
+    var npcThumb = null
+    if (msg.body.event.text.indexOf('|') != -1) {
+      var commandParameters = msg.body.event.text.split('|')
+      npcName = commandParameters[0]
+    } else {
+      npcName = msg.body.event.text
+    }
+
     // testing persist data
-    kv.set('u::c::jenkins', { name: 'Jenkins', thumb: 'https://u8921732.dl.dropboxusercontent.com/u/8921732/slack/thumbs/ar_jenkins.png' }, function (err) {
+    kv.set(`${userID}::NPC::${npcName}`, { name: npcName, thumb: npcThumb }, function (err) {
       //[TO DO] handle error
     })
   })
 }
+
+//'https://u8921732.dl.dropboxusercontent.com/u/8921732/slack/thumbs/ar_jenkins.png'
